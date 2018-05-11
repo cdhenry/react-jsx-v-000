4bb2adc1d084e17e73d4776c47cf9a1981e0dd2f
Chris Henry <cdhenry@gmail.com>
2018-05-11 00:13:21 +0000

    Done.

---
+++ src/components/fruits.js
@@ -0,0 +1,12 @@
+export default {
+  apple: 'red',
+  banana: 'yellow',
+};
+
+import Tweet from './Tweet';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <Tweet />,
+  document.getElementById('root')
+);
